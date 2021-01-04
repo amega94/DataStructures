68fc558c760fa95cae53fd3723381a755956e789
amega94 <amegahed94@gmail.com>
2021-01-04 10:20:31 +0200

    Create merge_v1.py

---
+++ merge/merge_v1.py
@@ -0,0 +1,30 @@
+### v1: MERGE BY VALUE 
+
+def merge(arr_1, arr_2):
+    i = j = 0
+    new = []
+    
+    # 'try-catch' to supress expecetion raised in base case when arr_1 or arr_2 is empty!
+    try:
+        if arr_1[i] > arr_2[j]:
+            new.append(arr_2[j])
+            j += 1
+        else:
+            new.append(arr_1[i])
+            i += 1
+    except Exception:
+        pass    
+        
+    if i == len(arr_1):
+        new += arr_2[j:]
+
+            
+    if j == len(arr_2):
+        new += arr_1[i:]
+    
+    return new
+
+arr_1 = [1]
+arr_2 = [10, 20, 30]
+
+print(merge(arr_1, arr_2))
